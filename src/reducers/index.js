71dc2f13f3a8a8f5367c5dbfa5b955d2c43665d1
Eugene <eugeneburlak@yandex.ru>
2019-03-15 22:53:50 +0300

    upd

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import userReducer from '../reducers/userReducer';
-import { routerReducer } from 'react-router-redux';
-
-export default combineReducers({
-	routing: routerReducer,
-	userReducer
-});
