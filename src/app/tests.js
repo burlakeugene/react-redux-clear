d4a6f6befcd1e32be42560a01e9d2bdb6444d1be
Eugene Burlak <burlakeugene@gmail.com>
2022-01-29 14:23:52 +0300

    some test examples

--- src/app/tests.js
+++ src/app/tests.js
@@ -11,4 +11,43 @@ let sum = function (num) {
     };
   return inner;
 };
-console.log(sum(2)(4)(10)());
+// console.log(sum(2)(4)(10)());
+
+// closure
+let inc = (() => {
+  let count = 0;
+  return () => {
+    return ++count;
+  };
+})();
+console.log(inc());
+console.log(inc());
+console.log(inc());
+
+let arr = ['bannana', 'bannana', 'grapefruit', 'orange', 'orange', 'bannana'];
+
+let sort = (arr) => {
+  let result = arr.reduce((acc, item) => {
+    acc[item] = (acc[item] || 0) + 1;
+    return acc;
+  }, {});
+  result = Object.entries(result);
+  result = result
+    .sort((a, b) => {
+      return b[1] - a[1];
+    })
+    .map((item) => item[0]);
+  return result;
+};
+
+// console.log(sort(arr));
+
+let multiplyByTwo = (...args) => {
+  let result = [];
+  for(let i = 0; i <= args.length - 1; i++){
+    result.push(args[i] * 2);
+  }
+  return result;
+};
+
+// console.log(multiplyByTwo(4, 8, 1));
